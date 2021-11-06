dd0414dd3cbaa75d9a0bb183516025758cdef670
Danilo <dabasantaco@gmail.com>
2021-11-06 17:56:38 -0500

    Filesystem Checks incomplete.

--- AL2.sh
+++ AL2.sh
@@ -15,6 +15,13 @@ banner() {
   local n=0
 }
 
+passed="\e[1m\e[92m"
+fail="\e[1m\e[91m"
+end="\e[0m"
+good="[\e[92m+${end}]"
+bad="[\e[91m+${end}]"
+slp="sleep 0.2"
+
 getTime() {
   local datestamp
   datestamp=$(date)
@@ -23,28 +30,293 @@ getTime() {
 
 initCSV() {
   local content="ID,NAME,SCORE"
-  local report="CIS-report.csv"
-  if [[ -w . ]]; then
-    touch CIS-report.csv
-    return $report
-  else
-    touch /tmp/CIS-report.csv
-    local report="/tmp/CIS-report.csv"
-    return $report
-  fi
+  report="/tmp/CIS-report.csv"
+  touch $report
+  echo $content > $report
 }
-
-reportName=initCSV
 
 checkL1() {
-  modprobe -n -v cramfs >/dev/null 2>&1
+  local checks=0
+  local counter=0
+
+  modprobe -n -v cramfs > /dev/null 2>&1
   if [ $? -eq 0 ]; then
     local out="FAIL"
+    echo -e "${bad} 1.1.1.1 Ensure mounting of cramfs filesystems is disabled [${fail}${out}${end}]"
   else
     local out="PASS"
+    echo -e "${good} 1.1.1.1 Ensure mounting of cramfs filesystems is disabled [${passed}${out}${end}]"
+    counter=$((counter+1))
   fi
-  echo "1.1.1.1, Ensure mounting of cramfs filesystems is disabled, $out" >> $reportName
-  echo -e "1.1.1.1 Ensure mounting of cramfs filesystems is disabled [${out}]"
+  echo "1.1.1.1, Ensure mounting of cramfs filesystems is disabled, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  modprobe -n -v hfs > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="FAIL"
+    echo -e "${bad} 1.1.1.2 Ensure mounting of hfs filesystems is disabled [${fail}${out}${end}]"
+  else
+    local out="PASS"
+    echo -e "${good} 1.1.1.2 Ensure mounting of hfs filesystems is disabled [${passed}${out}${end}]"
+    counter=$((counter+1))
+  fi
+  echo "1.1.1.2, Ensure mounting of hfs filesystems is disabled, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  modprobe -n -v hfsplus > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="FAIL"
+    echo -e "${bad} 1.1.1.3 Ensure mounting of hfsplus filesystems is disabled [${fail}${out}${end}]"
+  else
+    local out="PASS"
+    echo -e "${good} 1.1.1.3 Ensure mounting of hfsplus filesystems is disabled [${passed}${out}${end}]"
+    counter=$((counter+1))
+  fi
+  echo "1.1.1.3, Ensure mounting of hfsplus filesystems is disabled, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  modprobe -n -v squashfs > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="FAIL"
+    echo -e "${bad} 1.1.1.4 Ensure mounting of squashfs filesystems is disabled [${fail}${out}${end}]"
+  else
+    local out="PASS"
+    echo -e "${good} 1.1.1.4 Ensure mounting of squashfs filesystems is disabled [${passed}${out}${end}]"
+    counter=$((counter+1))
+  fi
+  echo "1.1.1.4, Ensure mounting of squashfs filesystems is disabled, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  modprobe -n -v udf > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="FAIL"
+    echo -e "${bad} 1.1.1.5 Ensure mounting of udf filesystems is disabled [${fail}${out}${end}]"
+  else
+    local out="PASS"
+    echo -e "${good} 1.1.1.5 Ensure mounting of udf filesystems is disabled [${passed}${out}${end}]"
+    counter=$((counter+1))
+  fi
+  echo "1.1.1.5, Ensure mounting of udf filesystems is disabled, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /tmp > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.2 Ensure /tmp is configured [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.2 Ensure /tmp is configured [${fail}${out}${end}]"
+  fi
+  echo "1.1.2, Ensure /tmp is configured, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /tmp | grep nodev > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.3 Ensure nodev option set on /tmp partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.3 Ensure nodev option set on /tmp partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.3, Ensure nodev option set on /tmp partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /tmp | grep nosuid > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.4 Ensure nosuid option set on /tmp partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.4 Ensure nosuid option set on /tmp partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.4, Ensure nosuid option set on /tmp partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /tmp | grep noexec > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.5 Ensure noexec option set on /tmp partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.5 Ensure noexec option set on /tmp partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.5, Ensure noexec option set on /tmp partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  cat /etc/fstab | grep '/var' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.6 Ensure separate partition exists for /var [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.6 Ensure separate partition exists for /var [${fail}${out}${end}]"
+  fi
+  echo "1.1.6, Ensure separate partition exists for /var, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  cat /etc/fstab | grep '/var/tmp' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.7 Ensure separate partition exists for /var/tmp [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.7 Ensure separate partition exists for /var/tmp [${fail}${out}${end}]"
+  fi
+  echo "1.1.7, Ensure separate partition exists for /var/tmp, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep '/var/tmp' | grep 'nodev' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.8 Ensure nodev option set on /var/tmp partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.8 Ensure nodev option set on /var/tmp partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.8, Ensure nodev option set on /var/tmp partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep '/var/tmp' | grep 'nosuid' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.9 Ensure nosuid option set on /var/tmp partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.9 Ensure nosuid option set on /var/tmp partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.9, Ensure nosuid option set on /var/tmp partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep '/var/tmp' | grep 'noexec' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.10 Ensure noexec option set on /var/tmp partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.10 Ensure noexec option set on /var/tmp partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.10, Ensure noexec option set on /var/tmp partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  cat /etc/fstab | grep '/var/log' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.11 Ensure separate partition exists for /var/log [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.11 Ensure separate partition exists for /var/log [${fail}${out}${end}]"
+  fi
+  echo "1.1.11, Ensure separate partition exists for /var/log, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  cat /etc/fstab | grep '/var/log/audit' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.12 Ensure separate partition exists for /var/log/audit [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.12 Ensure separate partition exists for /var/log/audit [${fail}${out}${end}]"
+  fi
+  echo "1.1.12, Ensure separate partition exists for /var/log/audit, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep '/home' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.13 Ensure separate partition exists for /home [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.13 Ensure separate partition exists for /home [${fail}${out}${end}]"
+  fi
+  echo "1.1.13, Ensure separate partition exists for /home, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep '/home' | grep 'nodev' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.14 Ensure nodev option set on /home partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.14 Ensure nodev option set on /home partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.14, Ensure nodev option set on /home partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /dev/shm | grep 'nodev' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.15 Ensure nodev option set on /dev/shm partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.15 Ensure nodev option set on /dev/shm partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.15, Ensure nodev option set on /dev/shm partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /dev/shm | grep 'nosuid' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.16 Ensure nosuid option set on /dev/shm partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.16 Ensure nosuid option set on /dev/shm partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.16, Ensure nosuid option set on /dev/shm partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  mount | grep /dev/shm | grep 'noexec' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    local out="PASS"
+    echo -e "${good} 1.1.17 Ensure noexec option set on /dev/shm partition [${passed}${out}${end}]"
+    counter=$((counter+1))
+  else
+    local out="FAIL"
+    echo -e "${bad} 1.1.17 Ensure noexec option set on /dev/shm partition [${fail}${out}${end}]"
+  fi
+  echo "1.1.17, Ensure noexec option set on /dev/shm partition, $out" >> $report
+  checks=$((checks+1))
+  $slp
+
+  echo $checks
+  echo $counter
 
 
 }
+
+initCSV
+checkL1
